9e29bf964bfeef565bd11f4152005032c976cf7e
DmitryM <rf00rm@gmail.com>
2020-08-07 12:51:26 +0700

    Minor approach proposal

--- src/client/models/world/generate-map.ts
+++ src/client/models/world/generate-map.ts
@@ -6,33 +6,38 @@ interface MapConfig {
 }
 
 interface Node {
-  x: number
-  y: number
-  z: number
+  position: DecartCords // stands for xyz
   nodeSize: number
   id: string
   color: string
 }
 
-export const generateMap = ({ nodeSize, axisCapacity }: MapConfig) => {
-  const nodesByID = {} as { [id: string]: Node }
-  const zMap = new Map()
-  for (let z = 0; z <= axisCapacity; z++) {
-    zMap.set(z, new Map())
-  }
-  zMap.forEach((yMap, z) => {
+type CubeRecord = { vectorIdx: number; nodeId: string }
+type Cube = CubeRecord[][][]
+
+function createRandomColor(): string {
+  return Math.floor(Math.random() * 16777215).toString(16)
+}
+
+export function createWorld({ nodeSize, axisCapacity }: MapConfig) {
+  const cube: Cube = []
+  const nodes: Node[] = []
+  for (let x = 0; x <= axisCapacity; x++) {
+    cube.push([])
     for (let y = 0; y <= axisCapacity; y++) {
-      yMap.set(y, new Map())
-    }
-    yMap.forEach((xMap: any, y: any) => {
-      for (let x = 0; x <= axisCapacity; x++) {
-        const id = nanoid()
-        const randomColor = Math.floor(Math.random() * 16777215).toString(16)
-        const node = { x, y, z, nodeSize, id, color: `#${randomColor}` }
-        xMap.set(id, node)
-        nodesByID[id] = node
+      cube[x].push([])
+      for (let z = 0; z <= axisCapacity; z++) {
+        const nodeId = nanoid()
+        const node: Node = {
+          position: [x, y, z],
+          nodeSize,
+          id: nodeId,
+          color: `#${createRandomColor()}`,
+        }
+        nodes.push(node)
+        cube[x][y].push({ vectorIdx: nodes.length, nodeId })
       }
-    })
-  })
-  return { dimensionalMap: zMap, nodesByID }
+    }
+  }
+  return { cube, nodes }
 }
---
+++ src/client/service-types.d.ts
@@ -0,0 +1,1 @@
+type DecartCords = [number, number, number]
